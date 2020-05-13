7de18302af3654f55001838d0dfee118a43f5f35
catospeltincx <cato.speltincx@telenet.be>
2020-05-13 22:38:32 +0200

    pop up poging

--- js/fabric_dithering.js
+++ js/fabric_dithering.js
@@ -167,7 +167,7 @@ function onMouseWheel(e) {
   draw();
 }
 
-document.querySelectorAll(".library img").forEach((el) => {
+document.querySelectorAll(".library__child img").forEach((el) => {
   el.addEventListener("click", () => {
     fabric.Image.fromURL(el.src, (img) => {
       img.scale(0.2);
@@ -238,6 +238,33 @@ function resetZoom() {
   gX = 0;
   gY = 0;
 }
+
+// popup
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("popUp");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
 
 gOutputCanvas.addEventListener("mousedown", onMouseDown);
 gOutputCanvas.addEventListener("wheel", onMouseWheel);
