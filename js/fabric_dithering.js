239b77427aa5d78dc983eb7463578169e2fe3106
catospeltincx <cato.speltincx@telenet.be>
2020-06-01 20:55:33 +0200

    opmmaak overlay (half af)

--- js/fabric_dithering.js
+++ js/fabric_dithering.js
@@ -260,13 +260,15 @@ realFileBtn.addEventListener("change", (e) => {
 //HOW DOES THIS WORK BTN_____OVERLAY
 /* Open */
 function openNav() {
-  document.getElementById("myNav").style.width = "100%";
+  // document.getElementById("myNav").style.width = "100%";
   document.getElementById("myNav").style.display = "block";
+  //document.getElementById("myNav").style.visibility = "visible";
 }
 /* Close */
 function closeNav() {
-  document.getElementById("myNav").style.width = "0%";
+  // document.getElementById("myNav").style.width = "0%";
   document.getElementById("myNav").style.display = "none";
+  //document.getElementById("myNav").style.visibility = "hidden";
 }
 
 //
